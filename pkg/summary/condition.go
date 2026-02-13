19c457119bbc0f94e6f6990d4b853351a00a7648
Tom Lebreux <tom.lebreux@suse.com>
2026-02-13 07:34:59 -0500

    Revert "[2.14] use CAPI deprecated v1beta1 conditions if they exist (#618)"
    This reverts commit 9fb4be5d2b1aadb86238b54db684b3d78495e02a.

--- pkg/summary/condition.go
+++ pkg/summary/condition.go
@@ -9,53 +9,19 @@ import (
 	"github.com/rancher/wrangler/v3/pkg/data"
 )
 
-const capiAPIVersionV1Beta2 = "cluster.x-k8s.io/v1beta2"
-
 func GetUnstructuredConditions(obj map[string]interface{}) []Condition {
 	return getConditions(obj)
 }
 
 func getRawConditions(obj data.Object) []data.Object {
-	// For CAPI v1beta2 resources, use the deprecated v1beta1 conditions if they exist
-	// Otherwise, use standard status.conditions
-	conditions := getDeprecatedV1beta1Conditions(obj)
-	if len(conditions) == 0 {
-		conditions = obj.Slice("status", "conditions")
-	}
-
-	// Append conditions from cattle.io/status annotation
-	conditions = append(conditions, getAnnotationConditions(obj)...)
-
-	return conditions
-}
-
-// getDeprecatedV1beta1Conditions returns the deprecated v1beta1 conditions for CAPI v1beta2 resources.
-// Returns nil if not a CAPI v1beta2 resource or if no deprecated conditions exist.
-func getDeprecatedV1beta1Conditions(obj data.Object) []data.Object {
-	if obj.String("apiVersion") != capiAPIVersionV1Beta2 {
-		return nil
-	}
-	return obj.Slice("status", "deprecated", "v1beta1", "conditions")
-}
-
-// getAnnotationConditions extracts conditions from the cattle.io/status annotation.
-// Returns an empty slice if the annotation doesn't exist or cannot be parsed.
-func getAnnotationConditions(obj data.Object) []data.Object {
 	statusAnn := obj.String("metadata", "annotations", "cattle.io/status")
-	if statusAnn == "" {
-		return []data.Object{}
+	if statusAnn != "" {
+		status := data.Object{}
+		if err := json.Unmarshal([]byte(statusAnn), &status); err == nil {
+			return append(obj.Slice("status", "conditions"), status.Slice("conditions")...)
+		}
 	}
-
-	var status data.Object
-	if err := json.Unmarshal([]byte(statusAnn), &status); err != nil {
-		return []data.Object{}
-	}
-
-	conditions := status.Slice("conditions")
-	if conditions == nil {
-		return []data.Object{}
-	}
-	return conditions
+	return obj.Slice("status", "conditions")
 }
 
 func getConditions(obj data.Object) (result []Condition) {
@@ -104,31 +70,18 @@ func (c Condition) Equals(other Condition) bool {
 }
 
 func NormalizeConditions(runtimeObj runtime.Object) {
-	if runtimeObj == nil {
-		return
-	}
+	var (
+		obj           data.Object
+		newConditions []map[string]interface{}
+	)
 
 	unstr, ok := runtimeObj.(*unstructured.Unstructured)
 	if !ok {
 		return
 	}
 
-	obj := data.Object(unstr.Object)
-
-	// For CAPI v1beta2 resources, normalize the deprecated v1beta1 conditions if they exist
-	if deprecatedConditions := getDeprecatedV1beta1Conditions(obj); len(deprecatedConditions) > 0 {
-		normalizeAndSetConditions(obj, deprecatedConditions, "status", "deprecated", "v1beta1", "conditions")
-	}
-
-	// For all resources, normalize the standard status.conditions
-	if conditions := obj.Slice("status", "conditions"); len(conditions) > 0 {
-		normalizeAndSetConditions(obj, conditions, "status", "conditions")
-	}
-}
-
-func normalizeAndSetConditions(obj data.Object, conditions []data.Object, path ...string) {
-	var newConditions []interface{}
-	for _, condition := range conditions {
+	obj = unstr.Object
+	for _, condition := range obj.Slice("status", "conditions") {
 		var summary Summary
 		for _, summarizer := range ConditionSummarizers {
 			summary = summarizer(obj, []Condition{{Object: condition}}, summary)
@@ -139,10 +92,11 @@ func normalizeAndSetConditions(obj data.Object, conditions []data.Object, path .
 		if condition.String("lastUpdateTime") == "" {
 			condition.Set("lastUpdateTime", condition.String("lastTransitionTime"))
 		}
-		newConditions = append(newConditions, map[string]interface{}(condition))
+		newConditions = append(newConditions, condition)
 	}
 
 	if len(newConditions) > 0 {
-		obj.SetNested(newConditions, path...)
+		obj.SetNested(newConditions, "status", "conditions")
 	}
+
 }
